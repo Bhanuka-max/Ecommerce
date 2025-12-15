f564ba97308ae12b59f82015e01e01e27453a97b
raniduanjula <raniduanjula205@gmail.com>
2025-12-15 22:03:11 +0530

    Delete backend/src/main/java/com/example/backend/order/ENUMS directory

--- backend/src/main/java/com/example/backend/order/ENUMS/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.order.ENUMS;
-
-public enum OrderStatus {
-    PENDING, CONFIRMED, SHIPPED, DELIVERED, CANCELLED
-}
--- backend/src/main/java/com/example/backend/order/ENUMS/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.order.ENUMS;
-
-public enum PaymentStatus {
-    PAID, PENDING, FAILED
-}
